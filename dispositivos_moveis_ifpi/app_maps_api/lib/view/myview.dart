d6799010c90268051dd1208024592d07866ad406
Marcos1701 <catce.2022111tads0428@aluno.ifpi.edu.br>
2024-07-10 22:02:14 -0300

    Adicionando Detalhes :tada:

--- dispositivos_moveis_ifpi/app_maps_api/lib/view/myview.dart
+++ dispositivos_moveis_ifpi/app_maps_api/lib/view/myview.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
-
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
+// import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 // import 'package:geolocator/geolocator.dart';
 
@@ -37,20 +41,67 @@ class MapSampleState extends State<MapSample> {
     zoom: 18,
   );
 
-  Set<Marker> _getMarkers() {
-    return {
-      Marker(
-          markerId: const MarkerId('1'),
-          position: const LatLng(-5.093708656214135, -42.75412754927936),
-          infoWindow: const InfoWindow(title: 'Casa'),
-          icon: BitmapDescriptor.defaultMarkerWithHue(
-              BitmapDescriptor.hueYellow)),
-      const Marker(
-        markerId: MarkerId('2'),
-        position: LatLng(-5.088408, -42.81065),
-        infoWindow: InfoWindow(title: 'IFPI'),
-      ),
-    };
+  Uint8List? marketimages;
+  List<String> images = [
+    'assets/Logo-IFPI-Vertical.png',
+  ];
+
+  // created empty list of markers
+  final List<Marker> _markers = <Marker>[
+    Marker(
+        markerId: const MarkerId('1'),
+        position: const LatLng(-5.093708656214135, -42.75412754927936),
+        infoWindow: const InfoWindow(title: 'Casa'),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueYellow))
+  ];
+
+  // declared method to get Images
+  Future<Uint8List> getImages(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetHeight: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
+
+  final List<LatLng> _latLen = <LatLng>[
+    const LatLng(-5.088408, -42.81065),
+  ];
+
+  void getPermission() async {
+    await Geolocator.requestPermission();
+    return;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // initialize loadData method
+    loadData();
+    getPermission();
+  }
+
+  // created method for displaying custom markers according to index
+  loadData() async {
+    for (int i = 0; i < images.length; i++) {
+      final Uint8List markIcons = await getImages(images[i], 100);
+      // makers added according to index
+      _markers.add(Marker(
+        // given marker id
+        markerId: MarkerId(i.toString()),
+        // given marker icon
+        icon: BitmapDescriptor.fromBytes(markIcons),
+        // given position
+        position: _latLen[i],
+        infoWindow: const InfoWindow(
+          // given title for marker
+          title: 'IFPI',
+        ),
+      ));
+      setState(() {});
+    }
   }
 
   @override
@@ -62,11 +113,11 @@ class MapSampleState extends State<MapSample> {
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
         mapType: MapType.hybrid,
-        markers: _getMarkers(),
+        markers: Set<Marker>.of(_markers),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _changeLocation,
-        label: const Text('Simboraaaaa!!!'),
+        label: const Text('Change Location'),
         icon: const Icon(Icons.change_circle),
       ),
     );
